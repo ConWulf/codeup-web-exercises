fa577ca695bfc6626262b589dc782f3c6a3cb564
Connor Wulf <connor.m.wulf@gmail.com>
2020-09-25 13:37:18 -0500

    user interaction exercises

--- js/external-ex.js
+++ js/external-ex.js
@@ -2,8 +2,13 @@
 //console.log("this is an external js file");
 
 
-var myage = 50
-console.log(50);
+//var myAge = 50;
+//console.log(50);
 
 
 //alert("this is an alert");
+//var deleteYear = confirm("Do you want to delete 2020?");
+//console.log(deleteYear);
+
+//var favoriteAnimal = prompt("what is your favorite animal?");
+//console.log("your favorite animal is " + favoriteAnimal);
---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict"
+console.log("Hello from external js");
+
+//alert("Welcome to my Website");
+var favColor = prompt("What is your favorite color?");
+alert("Really, " + favColor + " is my favorite color too!");
