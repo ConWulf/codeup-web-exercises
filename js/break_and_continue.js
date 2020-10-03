ae22507da41fac54d35e7f63a6ba798ccd165348
Connor Wulf <connor.m.wulf@gmail.com>
2020-10-02 20:06:15 -0500

    starting to understand loops(with dumb moment)

---
+++ js/break_and_continue.js
@@ -0,0 +1,19 @@
+// var oddNumber = prompt("enter an odd number between 1 and 50");
+// while(oddNumber % 2 === 0 || isNaN(oddNumber)){
+//     if (typeof oddNumber === "object") {
+//         break;
+//     }
+//     oddNumber = prompt("enter an odd number between 1 and 50");
+// }
+//
+// console.log("the number to skip is: " + oddNumber);
+// for (var i = 0; i < 50; i++) {
+//     if (i === parseInt(oddNumber)) {
+//         console.log("Skipping: " + i);
+//         continue;
+//     }
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
---
+++ js/while.js
@@ -0,0 +1,23 @@
+
+// var i = 2
+// while(i < 65536){
+//     console.log(i *= 2);
+// }
+
+
+// var allCones = Math.floor(Math.random() * 50) + 50
+//
+// do {
+//  var conesBought = ((Math.floor(Math.random() * 5) + 1));
+//  if (allCones < conesBought) {
+//      console.log("I cannot sell you " + conesBought + " cones, I only have " + allCones + " cones left");
+//      break;
+//  } else if (allCones === conesBought) {
+//      console.log("I have sold all my cones!");
+//      break;
+//  }
+//     console.log(allCones + " cones left");
+//     console.log(conesBought + " cones bought");
+//     allCones-= conesBought
+// } while (allCones > 0);
+
