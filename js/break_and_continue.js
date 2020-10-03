046914c8a66441057ad63a70948b0c5e285d0320
Connor Wulf <connor.m.wulf@gmail.com>
2020-10-03 16:00:35 -0500

    narcissistic numbers and loop exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,30 @@
+
+//this works the same as th do/while;
 // var oddNumber = prompt("enter an odd number between 1 and 50");
-// while(oddNumber % 2 === 0 || isNaN(oddNumber)){
+// while(oddNumber % 2 === 0 || isNaN(oddNumber)) {
 //     if (typeof oddNumber === "object") {
 //         break;
 //     }
-//     oddNumber = prompt("enter an odd number between 1 and 50");
+//      oddNumber = prompt("enter an odd number between 1 and 50");
+//
 // }
+
+// do {
+//     if (typeof oddNumber === "object") {
+//         break;
+//     }
+//    var oddNumber = prompt("enter an odd number between 1 and 50");
+//
+// }while(oddNumber % 2 === 0 || isNaN(oddNumber))
 //
 // console.log("the number to skip is: " + oddNumber);
-// for (var i = 0; i < 50; i++) {
-//     if (i === parseInt(oddNumber)) {
+// for (var i = 1; i < 50; i++) {
+//     if (i === parseFloat(oddNumber)) {
 //         console.log("Skipping: " + i);
 //         continue;
 //     }
 //     if (i % 2 !== 0) {
-//         console.log(i);
+//         console.log("here is an odd number: " + i);
 //     }
 // }
 
