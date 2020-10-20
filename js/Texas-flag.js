226048770101c14f12ce9b8463e82b97cfde6c52
Connor Wulf <connor.m.wulf@gmail.com>
2020-10-20 09:04:29 -0500

    texas flag

--- js/Texas-flag.js
+++ js/Texas-flag.js
@@ -9,23 +9,22 @@ const height = blue.height;
 
 function star() {
     ctx.beginPath();
-    ctx.moveTo((width/2) - 10, (height/2) - 12);
+    ctx.moveTo((width/2) - 10, (height/2) - 15);
     ctx.lineTo((width/2) - 25, (height/2));
-    ctx.lineTo((width/2) - 65, (height/2));
+    ctx.lineTo((width/2) - 70, (height/2));
     ctx.lineTo((width/2) - 25, (height/2) + 10);
     ctx.lineTo((width/2) - 50, (height/2) + 25);
     ctx.lineTo((width/2) - 5, (height/2) + 15);
     ctx.lineTo((width/2) + 40, (height/2) + 25);
     ctx.lineTo((width/2) + 20, (height/2) + 10);
-    ctx.lineTo((width/2) + 40, (height/2));
+    ctx.lineTo((width/2) + 50, (height/2));
     ctx.lineTo((width/2) + 5 , (height/2));
-    ctx.lineTo((width/2) - 10, (height/2) - 12);
+    ctx.lineTo((width/2) - 10, (height/2) - 15);
     ctx.lineWidth = 1;
     ctx.fillStyle = "#fff"
     ctx.fill()
-    ctx.stroke();
 }
-
 star();
+
 
 })();
